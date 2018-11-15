605d89ff5a08d1657b0db894dec0271ac19c38aa
ArnoudVB <45067725+ArnoudVB@users.noreply.github.com>
2018-11-15 14:23:50 +0100

    Add files via upload
    start part b

---
+++ final_assignment.py
@@ -0,0 +1,32 @@
+
+#question b
+class Robot:
+
+    while self.is_finished() == False:
+        
+        def __init__(self):
+            self.cur_pos = [cur_row,cur_col]
+            self.last_pos =
+
+            self.last_dir =
+
+        def change_pos(self, pos):
+            self.last_pos = self.cur_pos
+            self.cur_pos.x = pos.x
+            self.cur_pos.y = pos.y
+
+        def walk_options(self):
+            return 
+        
+        def change_dir(self, dir):
+            self.last_dir = 
+
+        def is_finished(self):
+            if :
+                return True
+            else:
+                return False
+
+
+
+    
